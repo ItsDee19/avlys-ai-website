7abb6e65b32d9b88627650ca7e0fb1d9fae5dd9f
Gopal Jain <gopaljain@microsoft.com>
2025-07-11 10:38:04 +0530

    Fix failing UTs

--- jest.config.cjs
+++ jest.config.cjs
@@ -2,5 +2,6 @@ module.exports = {
   testEnvironment: 'jest-environment-jsdom',
   setupFilesAfterEnv: ['<rootDir>/jest.setup.cjs'],
   testMatch: ['**/__tests__/**/*.js?(x)', '**/?(*.)+(spec|test).js?(x)'],
+  testPathIgnorePatterns: ['/node_modules/', '/server/'],
   verbose: true,
 }; 
